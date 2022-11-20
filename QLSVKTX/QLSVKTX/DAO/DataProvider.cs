2a25ff666f47d6d6735b7814ea461dc3dd6e4076
Nguyễn Trần Hương <74103610+tranhuong2302@users.noreply.github.com>
2022-11-20 22:04:06 +0700

    Add MVC

---
+++ QLSVKTX/QLSVKTX/DAO/DataProvider.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace QLSVKTX.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
+            private set => instance = value;
+        }
+
+        private DataProvider() { }
+
+        private string connectionSTR = @"Data Source=.\SQLEXPRESS;Initial Catalog=QLSVKTX;Integrated Security=True"; // or @""; // sử dụng đễ truy vấn sql
+
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null) // trả về số dòng thành công
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR)) // using dùng để giải phóng dữ liệu
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection); // câu lệnh dùng để truy vấn từ sql
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return data;
+        }  //so dong thanh cong
+
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+            return data;
+        }  //return o dau tien
+    }
+}
