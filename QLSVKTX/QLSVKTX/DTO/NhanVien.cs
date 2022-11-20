e3e92b68a55f6786d1615f418e654317cc42dc3c
Nguyễn Trần Hương <74103610+tranhuong2302@users.noreply.github.com>
2022-11-20 22:22:58 +0700

    Add DAO DTO Resource folder l

---
+++ QLSVKTX/QLSVKTX/DTO/NhanVien.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+
+namespace QLSVKTX.DTO
+{
+    public class NhanVien
+    {
+        private string hoTen;
+        private DateTime? ngaySinh;
+        private string diaChi;
+        private string chucVu;
+        private string sdt;
+        public NhanVien(string hoTen, DateTime? ngaySinh, string diaChi, string chucVu, string sdt)
+        {
+            this.HoTen = hoTen;
+            this.NgaySinh = ngaySinh;
+            this.DiaChi = diaChi;
+            this.ChucVu = chucVu;
+            this.Sdt = sdt;
+        }
+        public NhanVien(DataRow row)
+        {
+            this.HoTen = row["hoten"].ToString();
+            this.NgaySinh = (DateTime?)row["ngaysinh"];
+            this.DiaChi = row["diachi"].ToString();
+            this.ChucVu = row["chucvu"].ToString();
+            this.Sdt = row["sdt"].ToString();
+        }
+
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public DateTime? NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public string ChucVu { get => chucVu; set => chucVu = value; }
+        public string Sdt { get => sdt; set => sdt = value; }
+    }
+}
